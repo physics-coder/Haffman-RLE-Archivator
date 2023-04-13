e5608c4580f51629e664248f80ad64b6b340c787
PhysicsCoder <mygolbraikh@gmail.com>
2023-04-13 20:09:38 +0300

    Implemented text-file archivation and unarchivation using the Huffman algorithm

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import tkinter as tk
 from tkinter import filedialog as fd
+from Huffman import Huffman
 def callback():
     name = fd.askopenfilename()
+    with open(name, 'r') as data:
+        huffer = Huffman()
+        if name[-3::] == "txt":
+            huffer.text_archive(data, "output")
+        elif name[-6::] == "ultarc":
+            huffer.text_unarchive("output", "unarchived")
+        exit(0)
 
 
 tk.Button(text='Click to Open File',
